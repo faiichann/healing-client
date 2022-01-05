65252f06464afc03a88c79d8f5ee1901499e8f3e
faiichannn <fainichkamon@gmail.com>
2022-01-05 23:12:17 +0700

    add function stage 2

--- src/pages/gamecontent/stage2.tsx
+++ src/pages/gamecontent/stage2.tsx
@@ -1,7 +1,81 @@
+import { Row, Col} from "antd";
+import { useEffect, useRef, useState } from "react";
+import CountDownTimer from "utils/countdownTimer";
+
 function GameStage2() {
+    let info = [
+        {text: 'smile' , value: 1},
+        {text: 'cry' , value: 0},
+        {text: 'happy' , value: 1},
+        {text: 'love' , value: 1},
+        {text: 'angry' , value: 0},
+        {text: 'satisfy' , value: 1}
+    ]
+    const [text, setText] = useState(0)
+    const [score, setScore] = useState(0)
+    // const [[hrs, mins, secs], setTime] = useState([0, 0, 10]);
+
+
+    const goodButton = (text:number) => {
+        setText(prev => (prev + 1 ) % info.length)
+        if (info[text].value === 1){
+            score < 10 ? setScore(score + 1) : passStage();
+        } else {
+            setScore(score)
+        }
+    }
+
+    const badButton = (text:number) => {
+        setText(prev => (prev + 1 ) % info.length)
+        if (info[text].value === 0){
+            score < 10 ? setScore(score + 1) : passStage();
+        } else {
+            setScore(score)
+        }
+    }
+
+    const passStage = () => {
+        alert('you pass')
+        setScore(0)
+    }
+
+    // useEffect(() => {
+    //         const tick = () => {
+    //             if (hrs === 0 && mins === 0 && secs === 0 && score < 10) {
+    //                 alert('you lose')
+    //                 reset()
+    //             }
+    //             else if (mins === 0 && secs === 0) {
+    //                 setTime([hrs - 1, 59, 59]);
+    //             } else if (secs === 0) {
+    //                 setTime([hrs, mins - 1, 59]);
+    //             } else {
+    //                 setTime([hrs, mins, secs - 1]);
+    //             }
+    //             };
+            
+    //             const reset = () => setTime([parseInt('0'), parseInt('0'), parseInt('10')]);
+    //             const timerId = setInterval(() => tick(), 1000);
+    //             return () => clearInterval(timerId);
+    // },[hrs, mins, score, secs]);
+
+    const hoursMinSecs = {hours: 0, minutes: 0, seconds: 10}
+    
     return (
        <>
-       <div>This is Game stage 2</div>
+       <p>Speed game</p>
+       <CountDownTimer  hoursMinSecs={hoursMinSecs}/>
+       {/* <div>
+                <p>{`${hrs.toString().padStart(2, '0')}:${mins
+                .toString()
+                .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p> 
+       </div> */}
+        <div><h1>{info[text].text}</h1></div>
+       <Row>
+           <Col><button onClick={()=>badButton(text)}>BAD</button></Col> 
+           <Col><button onClick={()=>goodButton(text)}>GOOD</button></Col>
+        </Row>
+        <h4>Point : {score}</h4>
        </>
     );
 }
