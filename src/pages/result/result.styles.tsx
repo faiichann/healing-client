dab30180d0ebed82458c1893c2cbf03641ba712c
faiichannn <fainichkamon@gmail.com>
2022-02-24 00:36:28 +0700

    update background card and dunction

--- src/pages/result/result.styles.tsx
+++ src/pages/result/result.styles.tsx
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components'
-import Common from 'assets/images/cards/Common.png'
+// import Common from 'assets/images/cards/Common.png'
 // import Rare from 'assets/images/cards/Rare.png'
 // import Epic from 'assets/images/cards/Epic.png'
 // import Legendary from 'assets/images/cards/Legendary.png'
@@ -10,22 +10,22 @@ interface CardsProps {
   }
 export const CardContainer = styled.div<CardsProps>`
     width: 285px;
-    height: 430px;
-    border-radius: 20px;
+    height: 440px;
+    /* border-radius: 20px; */
     box-sizing: border-box; 
-    background-image:  ${(props: CardsProps) => props.rank? `url('assets/images/cards/${props.rank}.png')` : `url(${Common})` };
+    background-image: ${(props: CardsProps) => props.rank? `url(${process.env.PUBLIC_URL}'cards/${props.rank}.png')` : `url(${process.env.PUBLIC_URL + 'cards/Common.png'})`};
     /* background-color: #FFFFFF; */
-    box-shadow: 0px 0px 83px -23px rgba(0, 0, 0, 0.25);
+    /* box-shadow: 0px 0px 83px -23px rgba(0, 0, 0, 0.25); */
     margin: 40px 16px 50px 16px;
-    padding: 10px;
+    padding: 25px;
     display: block;
     position: relative;
 `
 export const ImageContainer = styled.div`
     width: 80%;
-    height: 200px;
+    height: 150px;
     border-radius: 15px;
-    background: #b6b6b6;
+    background: transparent;
     margin: 20px;
     padding: 10px;
     display: block;
