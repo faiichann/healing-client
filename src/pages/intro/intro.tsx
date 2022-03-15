b9340bc7d5215188432419ac1a3887a337c138a7
faiichannn <fainichkamon@gmail.com>
2022-03-16 06:35:26 +0700

    add carousel image

--- src/pages/intro/intro.tsx
+++ src/pages/intro/intro.tsx
@@ -1,17 +1,21 @@
-import { Typography } from 'antd';
+import { Typography, Image } from 'antd';
 import Container from 'components/container/container'
 import { useHistory } from 'react-router-dom';
 import { Box, ButtonStyle } from 'theme/components';
 import { BoxSlide, BoxText, CarouselStyle, ContentStyle, ImageSlide, BoxContent } from './styles/intro.styles';
+import Intro1 from 'assets/images/stage/Intro1.png'
+import Intro2 from 'assets/images/stage/Intro2.png'
+import Intro3 from 'assets/images/stage/Intro3.png'
+import Intro4 from 'assets/images/stage/Intro4.png'
 const { Title, Text } = Typography;
 
 function Intro() {
     const history = useHistory();
     const data = [ 
-        {title: 'Healing Project' , des: 'เว็บไซต์นี้จัดทำขึ้นมาเพื่อพัฒนารูปแบบการมีส่วนร่วมผ่านเว็บแอปพลิเคชั่นแบบมีปฏิสัมพันธ์ด้วยกลไลเกมมิฟิเคชั่นสำหรับการต่อยอดทางธุรกิจสินทรัพย์ดิจิทัล ด้วยการสร้างเป้าหมายและแรงบันดาลใจในรูปแบบของ NFT card', img: '1'} , 
-        {title: 'Gamification' , des: 'เว็บไซต์นี้นำ game mechanics มาใช้กับสิ่งที่ไม่ใช่เกม เพื่อกระตุ้นให้คนมีพฤติกรรมตามเป้าหมาย ด้วยการจูงใจให้คนเกิดความสนุก ความท้าทาย ความตั้งใจเอาชนะ ด้วยวิธีและความรู้สึกแบบเดียวกับการเล่นเกม เช่นการCountdown จำนวนการ์ด, user level up, collection', img: '2'}, 
-        {title: 'NFT' , des: 'Non-Fungible Token หรือ NFT คือ Token ที่นำเทคโนโลยี Blockchain มาใช้ ทำให้สามารถแสดงความเป็นเจ้าของของสินทรัพย์นั้นๆ ได้ โดยแต่ละ Token จะแตกต่างกัน เช่น การ์ดเกม งานศิลปะ ของสะสม สามารถนำสินทรัพย์ที่มีความ Unique, Rare, Indivisible มาทำ Token ได้', img: '3'},
-        {title: 'How to play' , des: 'เลือกสิ่งที่ใช่สำหรับคุณ พร้อมกับระบุเป้าหมายที่ชัดเจน เพื่อนำไปสร้างรูปแบบการ์ดในแบบเฉพาะของคุณ การ์ดมีจำนวนจำกัดเพียง 200 ใบ รีบมาสร้างการ์ด NFT ที่ช่วยสร้างแรงบันดาลใจให้เป้าหมายของคุณกัน', img: '4'}
+        {title: 'Healing Project' , des: 'เว็บไซต์นี้จัดทำขึ้นมาเพื่อพัฒนารูปแบบการมีส่วนร่วมผ่านเว็บแอปพลิเคชั่นแบบมีปฏิสัมพันธ์ด้วยกลไลเกมมิฟิเคชั่นสำหรับการต่อยอดทางธุรกิจสินทรัพย์ดิจิทัล ด้วยการสร้างเป้าหมายและแรงบันดาลใจในรูปแบบของ NFT card', img: Intro1} , 
+        {title: 'Gamification' , des: 'เว็บไซต์นี้นำ game mechanics มาใช้กับสิ่งที่ไม่ใช่เกม เพื่อกระตุ้นให้คนมีพฤติกรรมตามเป้าหมาย ด้วยการจูงใจให้คนเกิดความสนุก ความท้าทาย ความตั้งใจเอาชนะ ด้วยวิธีและความรู้สึกแบบเดียวกับการเล่นเกม เช่นการCountdown จำนวนการ์ด, user level up, collection', img: Intro3}, 
+        {title: 'NFT' , des: 'Non-Fungible Token หรือ NFT คือ Token ที่นำเทคโนโลยี Blockchain มาใช้ ทำให้สามารถแสดงความเป็นเจ้าของของสินทรัพย์นั้นๆ ได้ โดยแต่ละ Token จะแตกต่างกัน เช่น การ์ดเกม งานศิลปะ ของสะสม สามารถนำสินทรัพย์ที่มีความ Unique, Rare, Indivisible มาทำ Token ได้', img: Intro4},
+        {title: 'How to play' , des: 'เลือกสิ่งที่ใช่สำหรับคุณ พร้อมกับระบุเป้าหมายที่ชัดเจน เพื่อนำไปสร้างรูปแบบการ์ดในแบบเฉพาะของคุณ การ์ดมีจำนวนจำกัดเพียง 200 ใบ รีบมาสร้างการ์ด NFT ที่ช่วยสร้างแรงบันดาลใจให้เป้าหมายของคุณกัน', img: Intro2}
     ];
     return (
         <Container header={{ title: 'Introduction', left: 'back' }}>
@@ -22,7 +26,10 @@ function Intro() {
         return (
                 <ContentStyle key={index}>
                     <ImageSlide>
-                        {item.img}
+                    <Image
+                        width={500}
+                        src={item.img}
+                    />  
                     </ImageSlide>
                     <BoxText>
                     <Title level={5}>{item.title}</Title>
