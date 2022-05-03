fa1e6bf5f6ce11807ee099c9ec144445e16b847a
faiichannn <fainichkamon@gmail.com>
2022-05-04 02:07:02 +0700

    track Ga

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import { AppProvider } from 'context/appContext';
 import GlobalStyle from 'theme/globalStyle';
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-KC9WRM8'
+};
+
+TagManager.initialize(tagManagerArgs)
 
 function App() {
   return (
